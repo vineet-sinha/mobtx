a67ca23b3ee5aeefce60c7f122439dcb98d231b3
Vineet Sinha <vineet.sinha@gmail.com>
2016-10-08 22:53:06 -0400

    ask the user for a url

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -8,8 +9,34 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public alertCtrl: AlertController) {
 
   }
-
+  ionViewDidLoad() {
+    let prompt = this.alertCtrl.create({
+      title: 'Load Url',
+      message: "Enter a url for the service to load",
+      inputs: [
+        {
+          name: 'url',
+          placeholder: 'http://www.cnn.com'
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          handler: data => {
+            console.log('Cancel clicked: ' + data.url);
+          }
+        },
+        {
+          text: 'Save',
+          handler: data => {
+            console.log('Saved clicked: ' + data.url);
+          }
+        }
+      ]
+    });
+    prompt.present();
+  }
 }
