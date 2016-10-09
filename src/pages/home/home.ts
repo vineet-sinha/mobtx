659c5a7400614fdc1e7e4304764fbf621283be6f
Vineet Sinha <vineet.sinha@gmail.com>
2016-10-09 15:46:38 -0400

    get the http request working

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,4 +1,14 @@
 import { Component } from '@angular/core';
+import { Http, Response }    from '@angular/http';
+
+// import 'rxjs/Rx'; // <-- will add all rxjs operators
+// import { Observable }     from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+// import 'rxjs/operator/delay';
+// import 'rxjs/operator/mergeMap';
+// import 'rxjs/operator/switchMap';
+
 
 import { NavController } from 'ionic-angular';
 import { AlertController } from 'ionic-angular';
@@ -11,8 +21,9 @@ export class HomePage {
 
   url: string;
 
-  constructor(public navCtrl: NavController, public alertCtrl: AlertController) {
-    this.url = 'http://www.cnn.com';
+  constructor(private navCtrl: NavController, private alertCtrl: AlertController, private http: Http) {
+    // this.url = 'http://www.cnn.com';
+    this.url = '/proxy';
   }
 
   ionViewDidLoad() {
@@ -43,11 +54,33 @@ export class HomePage {
         }
       ]
     });
-    prompt.present();
+    // prompt.present();
+    this.loadUrl(); // <-- short cut so that we can test loading the page
   }
 
-  loadUrl() {
+  private loadUrl() {
     console.log('loading ' + this.url);
+    this.http.get(this.url).map(this.parseUrl).catch(this.handleError)
+      .subscribe(
+        data => console.log('urls', data.urls),
+        error => console.log('err:', error)
+        )
+  }
+
+  private parseUrl(res: Response) {
+    var urls = res.text().match(/['\"]https?:[^\s]+['\"]/g);
+    urls = urls
+            .filter(str => str.indexOf("image")!=-1)
+            .map(str => str.substring(1, str.length-1) );
+    console.log('urls.length:', urls.length);
+    return {urls: urls};
+  }
+
+  private handleError(error: any) {
+    let errMsg = (error.message) ? error.message :
+      error.status ? `${error.status} - ${error.statusText}` : 'Server error';
+    console.error('err:', errMsg); // log to console instead
+    return Observable.throw(errMsg);
   }
 
 }
