f8f3d1d5401087ea07712a1ba4985b4539a77085
Vineet Sinha <vineet.sinha@gmail.com>
2016-10-15 23:53:45 -0400

    overhault to support going for additional pages

--- src/pages/home/tx.service.ts
+++ src/pages/home/tx.service.ts
@@ -14,12 +14,44 @@ import 'rxjs/add/operator/catch';
 export class TxService {
   constructor (private http: Http) {}
 
-  public getImgUrls(url: string) {
-    return this.http.get(url).map(this.parseUrl).catch(this.handleError);
+  public getImgUrls(url: string): Observable<string[]> {
+    return Observable.create(observer => {
+      this.loadUrl(observer, url, []);
+    }).map(this.extractContent).catch(this.handleError);
   }
 
-  private parseUrl(res: Response) {
+  private loadUrl(observer, url, done) {
+    this.http.get(url).subscribe(res => {
+      this.checkNextPage(res, observer, url, done);
+      observer.next(res); // push content for next process (the extractContent)
+    });
+  }
+
+  private checkNextPage(res, observer, url, done) {
+    var next = res.text().match(/href=.[^"]*page[^"]*. class=.btn btn-default./g);
+    if (next==null) return; // no next page
+    if (Object.keys(done).length>=10) return; // already loaded 10 pages
+
+    next = next.map(nu => nu.substring("href=".length+1, nu.indexOf(" class")-1) );
+    var nextUrl = next.find(nu => !done[nu] );
+
+    var baseUrl = url.match(/^[^:]*:..[a-zA-Z.]*\//);
+    if (!baseUrl) baseUrl = url.match(/^[^?]*/);
+    baseUrl = baseUrl.map(s => { return s.endsWith('/') ? s.slice(0, -1) : s });
+
+    console.log('next url:', baseUrl[0] + nextUrl);
+
+    done[nextUrl] = true;
+    this.loadUrl(observer, baseUrl[0] + nextUrl, done);
+  }
+
+  private extractContent(res: Response) {
+    if (!res) {
+      console.log('res is not defined');
+      return {urls: ['res is not defined'], next: ['res is not defined']};
+    }
     var urls = res.text().match(/['\"]https?:[^\s]+['\"]/g);
+    if (urls == null) urls = [];
     urls = urls
             .filter(str => str.indexOf("image")!=-1)
             .filter(str => str.indexOf("30x30")==-1)
@@ -33,7 +65,7 @@ export class TxService {
   private handleError(error: any) {
     let errMsg = (error.message) ? error.message :
       error.status ? `${error.status} - ${error.statusText}` : 'Server error';
-    console.error('err:', errMsg); // log to console instead
+    console.error('err:', errMsg, error.stack);
     return Observable.throw(errMsg);
   }
 }
